8069e0d9a304001354d4ead539df0815126eba41
liubin <752791962@qq.com>
2017-07-13 18:48:08 +0800

    12321

---
+++ js/m.touch.js
@@ -0,0 +1,30 @@
+$(function(){
+// 商品详情页	
+	//详情主体切换
+	$('ul.switch_inner>li').tap(function(){
+		if ($(this).hasClass("current")) { 
+					return false;
+				}else {
+					$(this).parent().find('li.current').removeClass('current');
+					$(this).addClass('current');
+					$('section.details_main').hide().eq($(this).index()).show();
+					 h = $('section.details_main').eq($(this).index()).height();
+					$('.car_show_main_box').height(h);
+				}
+		});
+	//数量加减
+	$('button.count_r').tap(function(){
+		var val = $(this).parent().find('input').val();
+		if ( parseInt(val) == 1 )  { 
+			return 
+		} else { 
+			val = parseInt(val) - 1;
+			$(this).parent().find('input').val(val);
+		}
+	});
+	$('button.count_add').tap(function(){
+		var val = $(this).parent().find('input').val();
+			val = parseInt(val) + 1;
+			$(this).parent().find('input').val(val);
+	});
+})
