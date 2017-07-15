50d84c66a93d05bc9836a55f29818c1dfda29ece
liubin <752791962@qq.com>
2017-07-15 14:26:01 +0800

    7-15

--- js/m.runing.js
+++ js/m.runing.js
@@ -1,3 +1,25 @@
+ function break_url(){
+ 	if (document.referrer === '') {
+ 		$('a.link_referrer').attr('href', '');
+ 	}
+ }
+ var area1;//定义选择城市变量
+ 
+function select_city(){
+		if ( $('#manage_addr').length > 0 ) {
+			 area1 = new LArea();
+				area1.init({
+				'trigger': '#manage_addr', //触发选择控件的文本框，同时选择完毕后name属性输出到该位置
+				'valueTo': '#value1', //选择完毕后id属性输出到该位置
+				'keys': {
+					id: 'id',
+					name: 'name'
+				}, //绑定数据源相关字段 id对应valueTo的value属性输出 name对应trigger的value属性输出
+				'type': 1, //数据源类型
+				'data': LAreaData //数据源
+			});
+		} else return;
+	};
 $(function(){
 	var details_img_slide = new Swiper( '.swiper-details', {
 		  loop: true,
@@ -5,15 +27,7 @@ $(function(){
 		  pagination: '.swiper-pagination'
 	});
 	// 管理收货地址
-	var area1 = new LArea();
-	area1.init({
-		'trigger': '#manage_addr', //触发选择控件的文本框，同时选择完毕后name属性输出到该位置
-		'valueTo': '#value1', //选择完毕后id属性输出到该位置
-		'keys': {
-			id: 'id',
-			name: 'name'
-		}, //绑定数据源相关字段 id对应valueTo的value属性输出 name对应trigger的value属性输出
-		'type': 1, //数据源类型
-		'data': LAreaData //数据源
-	});
+
+	select_city()
+	
 })
--- js/m.touch.js
+++ js/m.touch.js
@@ -1,3 +1,33 @@
+
+function creat_address(){
+		var $box = $('section.creat_contact_info');
+		var name = $box.find('li.creat_user>input').val();
+		var mobile = $box.find('li.creat_tel>input').val();
+		var address = $box.find('li.creat_address>input').val();
+		var addr_details = $.trim($box.find('li.creat_address_details>input').val());
+		var RE_phone = /^1[34578][\d]{9}$/;
+		if ( name == '' || name.length == 0 ) {
+				$('div.tip_box>.tip_box_txt').html('请填写收件人姓名');
+				$('div.tip_mask, div.tip_box').css({'display':'block'});
+				return false;
+			}else if( !RE_phone.test( mobile ) ) {
+				if ( mobile == '' || mobile.length == 0 ) {
+					$('div.tip_box>.tip_box_txt').html('请填写收件人手机号码');
+					$('div.tip_mask, div.tip_box').css({'display':'block'});
+					return false;
+				}else{
+					$('div.tip_box>.tip_box_txt').html('手机号码填写错误');
+					$('div.tip_mask, div.tip_box').css({'display':'block'});
+					return false;
+				}	
+			}else if( addr_details == '' || addr_details.length < 3){
+				$('div.tip_box>.tip_box_txt').html('请填写详细收货地址');
+				$('div.tip_mask, div.tip_box').css({'display':'block'});
+				return false;
+			}else { 
+				return true;
+			}
+	}
 $(function(){
 // 商品详情页	
 	//详情主体切换
@@ -82,14 +112,23 @@ $(function(){
 		}else {
 			var complete_addr = address + addr_details;//完整地址
 			$('div._order_user').html(name);
+			$('input._order_user').val(name);
 			$('div._order_tel').html(mobile);
+			$('input._order_tel').val(mobile);
 			$('p._order_address').html(complete_addr);
-			$('section.manage_contact_info').css({'display':'none'});
-			$('section.part_hidde').css({'display':'block'});
+			$('input._order_address').val(complete_addr);
+			$('section.manage_contact_info, .manage_contact_head').css({'display':'none'});
+			$('.part_hidde').css({'display':'block'});
 		}
+	});
+//创建收货地址
+//取消订单页
+	//取消订单
+	$('button.cancel_order').tap(function(){
+		$('div.tip_mask, div.tip_box').css({'display':'block'});
 	});
 //弹框关闭
 	$('a.tip_box_btn.return').tap(function(){
 		$('div.tip_mask, div.tip_box').css({'display':'none'});
-	})
+	})	
 })
